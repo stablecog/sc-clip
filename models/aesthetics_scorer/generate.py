2922d3dfb932cba68d2172f3813ef899901c565b
yekta <ngyekta@gmail.com>
2024-08-05 16:16:24 +0300

    Pass calculated embeddings from CLIP to aesthetic scorer

--- models/aesthetics_scorer/generate.py
+++ models/aesthetics_scorer/generate.py
@@ -24,21 +24,23 @@ def normalize(value, range_min, range_max):
 
 
 def generate_aesthetic_scores(
-    image, aesthetics_scorer: AestheticsScorer, clip: OpenCLIP
+    image, aesthetics_scorer: AestheticsScorer, clip: OpenCLIP, embedding=None
 ) -> AestheticScoreResult:
     clip_processor = clip.processor
     vision_model = clip.model.vision_model
     rating_model = aesthetics_scorer.rating_model
     artifacts_model = aesthetics_scorer.artifacts_model
+    calculated_embedding = embedding
 
-    inputs = clip_processor(images=image, return_tensors="pt").to(DEVICE_CPU)
+    if calculated_embedding is None:
+        inputs = clip_processor(images=image, return_tensors="pt").to(DEVICE_CPU)
+        with torch.no_grad():
+            vision_output = vision_model(**inputs)
+        pooled_output = vision_output.pooler_output
+        calculated_embedding = preprocess(pooled_output)
     with torch.no_grad():
-        vision_output = vision_model(**inputs)
-    pooled_output = vision_output.pooler_output
-    embedding = preprocess(pooled_output)
-    with torch.no_grad():
-        rating = rating_model(embedding)
-        artifact = artifacts_model(embedding)
+        rating = rating_model(calculated_embedding)
+        artifact = artifacts_model(calculated_embedding)
     return AestheticScoreResult(
         rating_score=normalize(rating.detach().cpu().item(), 0, 10),
         artifact_score=normalize(artifact.detach().cpu().item(), 0, 5),
